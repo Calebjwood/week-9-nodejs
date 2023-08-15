b6d89f6aacc5f991e43f63eb2fab0b306077d26f
Caleb Wood <Calebwjwood60@gmail.com>
2023-08-15 05:51:58 -0500

    badge working and creating README from program

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,7 +10,7 @@ function renderLicenseBadge(license) {
    else if(license === "UNLICENSED"){
     return ''
    } else{
-    const badge = license.split(" ").join("20%")
+    const badge = license.split(" ").join("%20")
     
     return badge
    }
@@ -33,17 +33,29 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  
+        if(license === 'ISC'){
+         let licenseSection = 
+         `Copyright (c)\n
+          Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above\n
+          copyright notice and this permission notice appear in all copies.\nTHE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL\n
+          IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,\n
+          INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN \n
+          AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR \n
+          PERFORMANCE OF THIS SOFTWARE.
+          `
+         return licenseSection
+         
+        }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
-  renderLicenseLink(answers.license)
-  renderLicenseSection(answers.license)
+
   return `# ${answers.title}
-  <a href ="https://opensource.org/licenses/${renderLicenseLink(answers.license)}">
-  <img src="https://img.shields.io/badge/License-${renderLicenseBadge(answers.license)}" alt="license">
-  <a/>
+  ![Static Badge](https://img.shields.io/badge/License-${renderLicenseBadge(answers.license)}-blue)\n
+  https://opensource.org/licenses/${renderLicenseLink(answers.license)}
+  
+
 `;
 }
 
